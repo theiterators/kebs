f26dc33e622b0071514b149b5bd9150fb350f95e
Zofia Partyka <zofiapartyka1@gmail.com>
2021-05-17 12:58:08 +0200

    Fix instances naming after merge

--- akka-http/src/test/scala/AkkaHttpMatchersTests.scala
+++ akka-http/src/test/scala/AkkaHttpMatchersTests.scala
@@ -4,7 +4,7 @@ import enumeratum.{Enum, EnumEntry}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import pl.iterators.kebs.instances.TimeInstances.{DayOfWeekNumber, ZonedDateTimeString}
+import pl.iterators.kebs.instances.TimeInstances.{DayOfWeekInt, ZonedDateTimeString}
 import pl.iterators.kebs.matchers._
 import pl.iterators.kebs.tag.meta.tagged
 import pl.iterators.kebs.tagged._
@@ -24,7 +24,7 @@ class AkkaHttpMatchersTests
     with ScalatestRouteTest
     with ScalaFutures
     with ZonedDateTimeString
-    with DayOfWeekNumber {
+    with DayOfWeekInt {
 
   test("Extract String instance") {
     val testRoute = path("test" / Segment.asString[ZonedDateTime]) { zdt =>
