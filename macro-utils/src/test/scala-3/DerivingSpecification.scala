cd3365b7784dd87927db7d1434997a4c7d1d13cc
Łukasz Sowa <contact@luksow.com>
2021-12-22 23:27:41 +0100

    Creating EnumOf typeclass (with a macro) to support Scala 3's enums.

--- macro-utils/src/test/scala-3/DerivingSpecification.scala
+++ macro-utils/src/test/scala-3/DerivingSpecification.scala
@@ -1,16 +1,22 @@
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Gen, Properties}
-
 import pl.iterators.kebs.macros.CaseClass1Rep
+import pl.iterators.kebs.macros.enums.EnumOf
 
-object DerivingSpecification extends Properties("CaseClass1Rep") {
-
-  import pl.iterators.kebs.support._
-
+object DerivingSpecification extends Properties("Deriving") {
   case class CC1Ex(whatever: String)
 
-  property("caseClass1Rep dervies properly from 1-element case class") = forAll { (stringValue: String) =>
+  property("CaseClass1Rep derives properly from 1-element case class") = forAll { (stringValue: String) =>
     val tc = implicitly[CaseClass1Rep[CC1Ex, String]]
     tc.apply(stringValue) == CC1Ex(stringValue) && tc.unapply(CC1Ex(stringValue)) == stringValue
+  }
+
+  enum Color {
+    case Red, Green, Blue
+  }
+
+  property("EnumOf derives properly for an enum") = forAll(Gen.oneOf(Color.values.toList)) { (color: Color) =>
+    val tc = implicitly[EnumOf[Color]]
+    tc.`enum`.values.contains(color) && tc.`enum`.valueOf(color.toString) == color && tc.`enum`.fromOrdinal(color.ordinal) == color
   }
 }
