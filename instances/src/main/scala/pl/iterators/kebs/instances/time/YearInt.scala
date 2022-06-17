0e70d05d6e22df2c254b39e89c6a93d30b1b8976
Łukasz Sowa <contact@luksow.com>
2022-06-17 17:23:25 +0200

    Changing Year instance representation - from String to Int.

---
+++ instances/src/main/scala/pl/iterators/kebs/instances/time/YearInt.scala
@@ -0,0 +1,14 @@
+package pl.iterators.kebs.instances.time
+
+import pl.iterators.kebs.instances.InstanceConverter
+import pl.iterators.kebs.instances.time.YearInt.YearFormat
+
+import java.time.Year
+
+trait YearInt {
+  implicit val yearFormatter: InstanceConverter[Year, Int] =
+    InstanceConverter[Year, Int](_.getValue, Year.of, Some(YearFormat))
+}
+object YearInt {
+  private[instances] val YearFormat = "ISO-8601 standard format e.g. 2007"
+}
