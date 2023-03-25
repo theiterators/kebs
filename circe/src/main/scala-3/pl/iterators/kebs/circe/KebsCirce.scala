a90e9f4ae100cea8ee5354d79582747f48567de1
Paweł Kiersznowski <pkiersznowski@iteratorshq.com>
2023-03-25 17:53:30 +0100

    adding abortImplicit mechanism, stab at supporting Capitalized format

--- circe/src/main/scala-3/pl/iterators/kebs/circe/KebsCirce.scala
+++ circe/src/main/scala-3/pl/iterators/kebs/circe/KebsCirce.scala
@@ -8,8 +8,12 @@ import io.circe.HCursor
 import pl.iterators.kebs.macros.CaseClass1Rep
 import pl.iterators.kebs.instances.InstanceConverter
 import io.circe.generic.AutoDerivation
+import scala.quoted.Type
+import io.circe.derivation.ConfiguredDecoder
+import io.circe.derivation.Configuration
 
 trait KebsCirce extends AutoDerivation {
+
    inline given[T, A](using rep: CaseClass1Rep[T, A], decoder: Decoder[A]): Decoder[T] =
     decoder.emap(obj => Try(rep.apply(obj)).toEither.left.map(_.getMessage))
 
@@ -25,17 +29,17 @@ trait KebsCirce extends AutoDerivation {
 
 object KebsCirce {
   trait NoFlat extends KebsCirce {
-    inline given[T <: Product]: Decoder[T] = ${KebsCirceMacros.NoflatVariant.materializeDecoder[T]}
-    inline given[T <: Product]: Encoder[T] = ${KebsCirceMacros.NoflatVariant.materializeEncoder[T]}
+  transparent inline given[T <: Product](using inline m: Mirror.Of[T]): Decoder[T] = KebsCirceMacros.NoflatVariant.materializeDecoder[T]
+  transparent inline given[T <: Product](using m: Mirror.Of[T]): Encoder[T] = KebsCirceMacros.NoflatVariant.materializeEncoder[T]
   }
 
   trait Snakified extends KebsCirce {
-    inline given[T <: Product]: Decoder[T] = ${KebsCirceMacros.SnakifyVariant.materializeDecoder[T]}
-    inline given[T <: Product]: Encoder[T] = ${KebsCirceMacros.SnakifyVariant.materializeEncoder[T]}
+    transparent inline given[T <: Product](using m: Mirror.Of[T]): Decoder[T] = KebsCirceMacros.SnakifyVariant.materializeDecoder[T]
+    transparent inline given[T <: Product](using m: Mirror.Of[T]): Encoder[T] = KebsCirceMacros.SnakifyVariant.materializeEncoder[T]
   }
 
   trait Capitalized extends KebsCirce {
-    inline given[T <: Product]: Decoder[T] = ${KebsCirceMacros.CapitalizedCamelCase.materializeDecoder[T]}
-    inline given[T <: Product]: Encoder[T] = ${KebsCirceMacros.CapitalizedCamelCase.materializeEncoder[T]}
+    transparent inline given[T <: Product](using inline m: Mirror.Of[T]): Decoder[T] = KebsCirceMacros.CapitalizedCamelCase.materializeDecoder[T]
+    transparent inline given[T <: Product](using m: Mirror.Of[T]): Encoder[T] = KebsCirceMacros.CapitalizedCamelCase.materializeEncoder[T]
   }
 }
--- circe/src/test/scala-3/CirceFormatNoFlatTests.scala
+++ circe/src/test/scala-3/CirceFormatNoFlatTests.scala
@@ -3,11 +3,14 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import pl.iterators.kebs.circe.KebsCirce
 import io.circe.parser.parse
+import io.circe.derivation.ConfiguredDecoder
+import io.circe.derivation.Configuration
+import model.ClassWith23Fields
 
 
 class CirceFormatNoFlatTests extends AnyFunSuite with Matchers {
   object KebsProtocol extends KebsCirce with KebsCirce.NoFlat
-  import KebsProtocol.{given, _}
+  import KebsProtocol._
 
 case class C(i: Int)
 
