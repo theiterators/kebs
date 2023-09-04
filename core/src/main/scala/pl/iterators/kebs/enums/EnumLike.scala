f90e0dc64a50e4674679de655038630bb18a53cf
agrodowski <agrodowski@iteratorshq.com>
2023-09-04 15:41:13 +0200

    Implement ValueEnumLike and EnumLike interfaces

--- core/src/main/scala/pl/iterators/kebs/enums/EnumLike.scala
+++ core/src/main/scala/pl/iterators/kebs/enums/EnumLike.scala
@@ -1,5 +1,13 @@
 package pl.iterators.kebs.enums
 
-trait EnumLike {
-
+trait EnumLike[T] {
+  def values: Array[T]
+  def valueOf(name: String): T = values.find(_.toString == name).getOrElse(throw new IllegalArgumentException(s"enum case not found: $name"))
+  def withNameUnsafe(name: String): T = values.find(_.toString == name).get
+  def withNameSafe(name: String): Option[T] = values.find(_.toString == name)
+  def valueOfIgnoreCase(name: String): T = values.find(_.toString.equalsIgnoreCase(name)).getOrElse(throw new IllegalArgumentException(s"enum case not found: $name"))
+  def withNameIgnoreCaseUnsafe(name: String): T = values.find(_.toString.equalsIgnoreCase((name))).get
+  def withNameIgnoreCaseSafe(name: String): Option[T] = values.find(_.toString.equalsIgnoreCase((name)))
+  def fromOrdinal(ordinal: Int): T = values.lift(ordinal).getOrElse(throw new NoSuchElementException(ordinal.toString))
+  def indexOf(member: T): Int = values.zipWithIndex.toMap.getOrElse(member, -1)
 }
--- core/src/main/scala/pl/iterators/kebs/enums/ValueEnumLike.scala
+++ core/src/main/scala/pl/iterators/kebs/enums/ValueEnumLike.scala
@@ -1,5 +1,14 @@
 package pl.iterators.kebs.enums
 
-trait ValueEnumLike {
+trait ValueEnumLikeEntry[ValueType] {
+  def value: ValueType
+}
 
+trait ValueEnumLike[ValueType, EntryType <: ValueEnumLikeEntry[ValueType]] {
+  def values: Array[EntryType]
+  def valueOf(value: ValueType): EntryType = values.find(enum => value == enum.value).getOrElse(throw new IllegalArgumentException(s"enum case not found: $value"))
+  def valueOfOpt(value: ValueType): Option[EntryType] = values.find(enum => value == enum.value)
+  def valueOfUnsafe(value: ValueType): EntryType = values.find(enum => value == enum.value).get
+  def fromOrdinal(ordinal: Int): EntryType = values.lift(ordinal).getOrElse(throw new NoSuchElementException(ordinal.toString))
+  def indexOf(member: EntryType): Int = values.zipWithIndex.find { case (enum, _) => member == enum }.map { case (_, index) => index }.getOrElse(-1)
 }
