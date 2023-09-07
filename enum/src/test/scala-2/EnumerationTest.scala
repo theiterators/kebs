97903777fcda25da2d8d49438906072d401e6ed6
agrodowski <agrodowski@iteratorshq.com>
2023-09-07 11:29:17 +0200

    Scala 2.12.18 support drop

---
+++ enum/src/test/scala-2/EnumerationTest.scala
@@ -0,0 +1,26 @@
+import MyEnum.MyEnum
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import pl.iterators.kebs.enums.EnumLike
+import pl.iterators.kebs.enums.KebsEnum
+import scala.collection.immutable
+import scala.reflect.runtime.universe.typeOf
+
+
+object MyEnum extends Enumeration {
+  type MyEnum = Value
+  val Value1, Value2, Value3 = Value
+}
+
+class EnumerationTest extends AnyFunSuite with Matchers with KebsEnum {
+
+  def toEnumLike(enumeration: MyEnum)(implicit convertEnumeration: MyEnum.Value => EnumLike[MyEnum.Value]): EnumLike[MyEnum.Value] =
+    convertEnumeration(enumeration)
+
+  val enumLike: EnumLike[MyEnum.MyEnum] = implicitly[EnumLike[MyEnum.MyEnum]]
+  test("EnumLike[MyEnum.Value].values should return all values of MyEnum") {
+    enumLike.values should contain theSameElementsAs Seq(MyEnum.Value1, MyEnum.Value2, MyEnum.Value3)
+
+    enumLike.valueOf("Value1") shouldEqual MyEnum.Value1
+  }
+}
