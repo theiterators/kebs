a0fe39840648ad71fa735506caa1ab871a50c149
Łukasz Sowa <contact@luksow.com>
2024-07-15 18:23:31 +0200

    given -> implicit for ValueClassReps

--- core/src/main/scala-3/pl/iterators/kebs/core/macros/ValueClassReps.scala
+++ core/src/main/scala-3/pl/iterators/kebs/core/macros/ValueClassReps.scala
@@ -5,7 +5,7 @@ import scala.deriving.Mirror
 final class ValueClassLike[VC, F1](val apply: F1 => VC, val unapply: VC => F1)
 
 trait CaseClass1ToValueClass {
-  inline given[T <: Product, F1](using m: Mirror.ProductOf[T], teq: m.MirroredElemTypes =:= F1 *: EmptyTuple.type): ValueClassLike[T, F1] =  {
+  implicit def repFromCaseClass[T <: Product, F1](using m: Mirror.ProductOf[T], teq: m.MirroredElemTypes =:= F1 *: EmptyTuple.type): ValueClassLike[T, F1] =  {
     new ValueClassLike[T, F1](f1 => m.fromProduct(Tuple1(f1)), _.productElement(0).asInstanceOf[F1])
   }
 }
