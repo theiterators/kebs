a0bbc3b13ea566fc671fbd379fa24e61e246a24d
Łukasz Sowa <contact@luksow.com>
2024-09-13 18:08:36 +0200

    Fixing #279. (#392)

--- scalacheck/src/main/scala/pl/iterators/kebs/scalacheck/Generators.scala
+++ scalacheck/src/main/scala/pl/iterators/kebs/scalacheck/Generators.scala
@@ -1,11 +1,12 @@
 package pl.iterators.kebs.scalacheck
 
-import org.scalacheck.Arbitrary
+import org.scalacheck.rng.Seed
+import org.scalacheck.{Arbitrary, Gen}
 
 trait Generator[T] extends CommonArbitrarySupport {
   def ArbT: Arbitrary[T]
 
-  def generate: T = ArbT.arbitrary.sample.get
+  def generate: T = ArbT.arbitrary.pureApply(Gen.Parameters.default, Seed.random())
 }
 
 trait AllGenerators[T] {
